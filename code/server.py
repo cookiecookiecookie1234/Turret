e02d69325ca0ce8c1a8fdc3c6d3fdb6aef2cf3f3
cookiecookiecookie1234 <giovanni_amaral@britishcollegebrazil.org>
2026-01-19 13:36:54 -0300

    detect faces
    im at the oftamologist cant seeee arghghhh     code will probably be scrapped just a simple test demo

--- code/server.py
+++ code/server.py
@@ -1,13 +1,30 @@
 from flask import Flask, request, render_template
 import time
+import os
+import cv2 as cv
+import sys
+import PIL as pil
 app = Flask(__name__)
+def faceDetect(imgpath):
+    img = cv.imread(imgpath)
+    grey_image = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    face_classifier = cv.CascadeClassifier(
+        cv.data.haarcascades + "haarcascade_frontalface_default.xml"
+    )
+    face = face_classifier.detectMultiScale(
+        grey_image, scaleFactor=1.01, minNeighbors=5, minSize=(40, 40)
+    )
+    if face:
+        return (face[0]+(face[2]/2),face[1]+(face[3]/2))
+    else:
+        return False
 @app.route('/',methods=['GET', 'POST'])
 def handleimage():
     if request.method == 'POST':
         image = request.get_data()
         with open("image.jpeg", 'wb') as myfile:
             myfile.write(image)
-        return "recieved"
+        return faceDetect("image.jpeg")
     return render_template('serverpage.html')
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0",port=5000)
